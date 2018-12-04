a67ac7521bb40f1bc24187cc5656ecb6608b3a08
Abu-Bakr Adam <asiddik5@gmail.com>
2018-12-04 20:12:19 +0100

    Modified add and subtract functions in calculator.py

--- calculator.py
+++ calculator.py
@@ -2,9 +2,9 @@ class Calculator:
     def __init__(self, *args):
         super().__init__(self, *args)
 
-    def addition(self, num1, num2):
+    def add(self, num1, num2):
         return num1 + num2
     
-    def subtraction(self, num1, num2):
+    def subtract(self, num1, num2):
         return num1 - num2
 
